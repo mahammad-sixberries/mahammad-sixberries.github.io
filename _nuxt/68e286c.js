20da06021868f1dd50f106d9371dbbf5d96b833d
Maddy <mahammad@sixberries.com>
2021-03-30 01:13:37 +0530

    commit

--- _nuxt/68e286c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{597:function(e,n,t){"use strict";t.r(n);var l={components:{Time:t(589).default}},c=t(76),component=Object(c.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("Time")}),[],!1,null,null,null);n.default=component.exports}}]);
